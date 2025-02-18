400536bcdeafd28dd6a494330d5d0461e291dbb4
Borges-Porteous <miguel.borges-porteous@cra-arc.gc.ca>
2025-02-18 14:15:17 -0500

    moving dropbox js to content-assistant

--- js/content-assistant.js
+++ js/content-assistant.js
@@ -1,5 +1,61 @@
 //Includes JS common to all of the content assistant tools
 $(document).ready(function() {
+
+  /*
+  $(function () {
+  var dropZoneId = "image-drop-upload-area";
+  var buttonId = "file-upload-btn";
+  var mouseOverClass = "mouse-over";
+
+  var dropZone = $("#" + dropZoneId);
+  var ooleft = dropZone.offset().left;
+  var ooright = dropZone.outerWidth() + ooleft;
+  var ootop = dropZone.offset().top;
+  var oobottom = dropZone.outerHeight() + ootop;
+  var inputFile = dropZone.find("input");
+  document.getElementById(dropZoneId).addEventListener("dragover", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  dropZone.addClass(mouseOverClass);
+  var x = e.pageX;
+  var y = e.pageY;
+
+  if (!(x < ooleft || x > ooright || y < ootop || y > oobottom)) {
+    inputFile.offset({ top: y - 15, left: x - 100 });
+  } else {
+    inputFile.offset({ top: -400, left: -400 });
+  }
+
+  }, true);
+
+  if (buttonId != "") {
+  var clickZone = $("#" + buttonId);
+
+  var oleft = clickZone.offset().left;
+  var oright = clickZone.outerWidth() + oleft;
+  var otop = clickZone.offset().top;
+  var obottom = clickZone.outerHeight() + otop;
+
+  $("#" + buttonId).mousemove(function (e) {
+    var x = e.pageX;
+    var y = e.pageY;
+    if (!(x < oleft || x > oright || y < otop || y > obottom)) {
+      inputFile.offset({ top: y - 15, left: x - 160 });
+    } else {
+      inputFile.offset({ top: -400, left: -400 });
+    }
+  });
+  }
+
+  document.getElementById(dropZoneId).addEventListener("drop", function (e) {
+  $("#" + dropZoneId).removeClass(mouseOverClass);
+  }, true);
+
+  })
+  */
+
+
+
   //tab interface toggle
   $('.tabs ul li a').on('click', function (e) {
     e.preventDefault();
