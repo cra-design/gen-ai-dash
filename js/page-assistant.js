dd76232da6d590ffe30dbc7e479e658970e600a7
Borges-Porteous <miguel.borges-porteous@cra-arc.gc.ca>
2025-02-18 15:46:38 -0500

    refresh button fix

--- js/page-assistant.js
+++ js/page-assistant.js
@@ -40,7 +40,7 @@ $(document).ready(function() {
   });
 
   $("#code-refresh-button").click(function(){
-    let iframeB = $("#url-frame")[0].contentDocument || $("#url-frame")[0].contentWindow.document;
+    let preview = $("#url-frame")[0].contentDocument || $("#url-frame")[0].contentWindow.document;
     preview.open();
     preview.write($('#fullHtml code').value);
     preview.close();
