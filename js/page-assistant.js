1944ac6101e31f0a9710e72149827c623b8e971f
Borges-Porteous <miguel.borges-porteous@cra-arc.gc.ca>
2025-02-27 14:53:31 -0500

    Update page-assistant.js

--- js/page-assistant.js
+++ js/page-assistant.js
@@ -325,13 +325,13 @@ $(document).ready(function() {
         return $(this).val();
       }).get();
       // Grab the report type from the <h4> element within the sidebyside-report
-      let reportType = $(this).closest('.toolbar').closest('.sidebyside-container').find('.sidebyside-report h4').text().trim();
+      let reportType = $(this).closest('.toolbar').closest().find('.sidebyside-report h4').text().trim();
 	  console.log($(this));
 	  console.log($(this).closest('.toolbar'));
-	  console.log($(this).closest('.toolbar').closest('.sidebyside-container'));
+	  console.log($(this).closest('.toolbar').closest());
 	  console.log($(this).parents('.sidebyside-container'));
-	  console.log($(this).closest('.toolbar').closest('.sidebyside-container').find('.sidebyside-report h4'));
-	  console.log($(this).closest('.toolbar').closest('.sidebyside-container').find('.sidebyside-report').find('h4'));
+	  console.log($(this).closest('.toolbar').closest().find('.sidebyside-report h4'));
+	  console.log($(this).closest('.toolbar').closest().find('.sidebyside-report').find('h4'));
 	  console.log(reportType);
 	  console.log(encodeURIComponent(reportType));
       // Redirect to Google Form with prefilled data
