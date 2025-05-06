a0733277d58f2857d1e7178f299e2fcc5a517d3b
RosaZheng1 <Rong.Zheng@cra-arc.gc.ca>
2025-05-06 09:36:26 -0400

    update AI model list

--- js/translation-assistant(docxOnly).js
+++ js/translation-assistant(docxOnly).js
@@ -714,10 +714,11 @@ $("#second-upload-btn").click(async function () {
     { role: "user", content: combinedPrompt }
   ];
 
-  const models = [    
+  const models = [   
     "google/gemini-2.0-flash-exp:free",
-    "google/gemini-exp-1206:free",
-    "google/gemini-flash-1.5-8b-exp",
+    "google/gemma-3-12b-it:free",
+    "google/gemma-3-4b-it:free",
+    "google/gemma-3-1b-it:free",
     "cognitivecomputations/dolphin3.0-r1-mistral-24b:free", 
     "cognitivecomputations/dolphin3.0-mistral-24b:free",
     "meta-llama/llama-3.3-70b-instruct:free",
