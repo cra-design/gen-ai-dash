e45484cec2e41f5ac0d98fc68efac0bb3d933ad6
RosaZheng1 <Rong.Zheng@cra-arc.gc.ca>
2025-05-06 13:57:23 -0400

    Update translation-assistant.js

--- js/translation-assistant.js
+++ js/translation-assistant.js
@@ -715,7 +715,7 @@ $("#second-upload-btn").click(async function () {
   ];
 
   const models = [   
-    "google/gemini-2.0-flash-exp:free",
+    "meta-llama/llama-4-maverick:free",
     "google/gemma-3-12b-it:free",
     "google/gemma-3-4b-it:free",
     "google/gemma-3-1b-it:free",
